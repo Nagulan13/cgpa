1f7300aa12119806fd9771a0ee7bb0a1a69a71a0
Nagulan <nagu1370@gmail.com>
2024-02-14 17:30:50 +0800

    a

---
+++ cgpa.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        body{
+            align-items: center,
+        }
+    </style>
+ <title>Calculate CGPA UTHM</title>
+</head>
+<body>
+<h1>UTHM CGPA CALCULATOR</h1>
+ <?php 
+ $name = "Muhammad Asyraf";
+ ?>
+ <table>
+    <tr>
+        <td></td>
+    </tr>
+ <tr>
+ <td>Number of subjects : </td>
+ <td><input type="text" name=""></input></td> 
+ </tr>
+ </table>
+ 
+</body>
+</html>
